744d24a3f1b53edf59fc941179f6fe7618d2b495
Kirill <ivanovk.952@gmail.com>
2023-11-20 02:23:00 +0300

    add files

---
+++ стек/TStack.h
@@ -0,0 +1,79 @@
+#pragma once
+template <class T>
+class Tstack
+{
+	T* pMem;
+	int Maxsize;
+	int Currind;
+public:
+
+	Tstack(int s = 10)
+	{
+		Maxsize = s;
+		pMem = new T[s];
+		Currind = -1;
+	}
+
+	~Tstack()
+	{
+		delete pMem;
+	}
+
+	Tstack(const Tstack& s)
+	{
+		Maxsize = s.Maxsize;
+		pMem = new T[Maxsize];
+		Currind = s.Currind;
+		for (int i = 0; i < Currind; i++)
+		{
+			pMem[i] = s.pMem[i];
+		}
+	}
+
+	bool empty()
+	{
+		if (Currind == -1) return true;
+		else return false;
+	}
+	bool full()
+	{
+		if (Currind == Maxsize - 1) return true;
+		else return false;
+	}
+	void push(const T& el)
+	{
+		pMem[Currind + 1] = el;
+		Currind++;
+		if (Currind >= Maxsize)
+			throw ("overload");
+	}
+
+	T pop()
+	{
+		if (Currind == -1)
+		{
+			throw("Stack is empty")
+		}
+		Currind--;
+		return pMem[Currind]// в тетради написано currind-1
+	}
+
+	T pop()
+	{
+		if (Currind == -1)
+		{
+			throw("Stack is empty")
+		}
+		return pMem[Currind];
+	}
+
+	void clear()
+	{
+		while (!s.empty())
+		{
+			s.pop();
+		}
+	}
+
+};
+
---
+++ стек/стек.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include"TStack.h"
+int BracketChecking(char *str)
+{
+Tstack<char> s(20);
+int res = 0;
+for (int i = 0; i < str.size(); i++)
+{
+	if (str[i] == '(')
+		s.push(str[i]);
+	if (str[i] == ')')
+	{
+		if (!s.empty())
+			s.pop();
+		else res = 1;
+	}
+}
+if (!s.empty())
+res = 2;
+}
+
+int main()
+{
+   
+}
+
